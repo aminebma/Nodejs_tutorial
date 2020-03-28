a136dfdfd2f94b5e3e10993a54d37a5db8d50122
Mohamed Amine BENBAKHTA <gm_benbakhta@esi.dz>
2020-03-28 22:18:15 +0100

    Templating the outputted html

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ var fs = require('fs');
 http.createServer(function(req,res){
 
     res.writeHead(200,{'Content-Type': 'text/html'});
-    var html = fs.readFileSync(__dirname +'/index.htm');
+    var html = fs.readFileSync(__dirname +'/index.htm', 'utf8');
+    var message = 'Hello world...';
+    html = html.replace('{Message}', message);
     res.end(html);
 
 }).listen(1337, '127.0.0.1');

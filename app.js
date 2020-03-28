97d7c314f1a0c3d83f72a535b5cc8d8fa4405f60
Mohamed Amine BENBAKHTA <gm_benbakhta@esi.dz>
2020-03-28 22:55:42 +0100

    Checking the entered url

--- app.js
+++ app.js
@@ -3,12 +3,18 @@ var fs = require('fs');
 
 http.createServer(function(req,res){
 
-    res.writeHead(200,{'Content-Type': 'application/json'});
-    var obj = {
-        firstname: 'Amine',
-        lastname: 'BENBAKHTA'
-    };
-    //Convertir en chaine
-    res.end(JSON.stringify(obj));
+    if(req.url === '/'){
+        fs.createReadStream(__dirname + '/index.htm').pipe(res);
+    }
 
+    if(req.url === '/api'){
+        res.writeHead(200,{'Content-Type': 'application/json'});
+        var obj = {
+            firstname: 'Amine',
+            lastname: 'BENBAKHTA'
+        };
+        //Convertir en chaine
+        res.end(JSON.stringify(obj));
+    }
+    
 }).listen(1337, '127.0.0.1');

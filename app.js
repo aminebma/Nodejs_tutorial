dfe1387d1d9f3e74210b1ef505cdd19ac4f1b92f
Mohamed Amine BENBAKHTA <gm_benbakhta@esi.dz>
2020-03-29 13:22:39 +0100

    Creating a tiny middleware

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ var port = process.env.PORT || 3000
 
 app.use('/assets', express.static(__dirname + '/public'))
 
+app.use('/', function(req, res, next){
+    console.log('Request URL: '+ req.url)
+    next()
+})
+
 app.get('/', function(req,res){
     res.send('<html><head><link href=assets/style.css type=text/css rel=stylesheet /></head><body><h1>Hello world !</h1></body></html>')
 })

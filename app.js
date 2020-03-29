906a66bac0604daabeea0c0f222fdf099cbd3d12
Mohamed Amine BENBAKHTA <gm_benbakhta@esi.dz>
2020-03-29 13:37:05 +0100

    Implementing the EJS Template Engine

--- app.js
+++ app.js
@@ -5,13 +5,15 @@ var port = process.env.PORT || 3000
 
 app.use('/assets', express.static(__dirname + '/public'))
 
+app.set('view engine', 'ejs')
+
 app.use('/', function(req, res, next){
     console.log('Request URL: '+ req.url)
     next()
 })
 
 app.get('/', function(req,res){
-    res.send('<html><head><link href=assets/style.css type=text/css rel=stylesheet /></head><body><h1>Hello world !</h1></body></html>')
+    res.render('index')    
 })
 
 app.get('/person/:id', function(req,res){

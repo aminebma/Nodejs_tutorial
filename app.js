242e482368bb5a713423bb3ed09393b68a08c16f
Mohamed Amine BENBAKHTA <gm_benbakhta@esi.dz>
2020-03-30 17:24:26 +0100

    connecting the frontend with the backend

--- app.js
+++ app.js
@@ -3,11 +3,20 @@ var app = express()
 
 var port = process.env.PORT || 3000
 
+var people = [
+    {
+        name: 'Amine BENBAKHTA'
+    },
+    {
+        name: 'Rosane BENKHELIL'
+    }
+]
+
 app.set('view engine', 'ejs')
 app.use('/assets', express.static(__dirname + '/public'))
 
 app.get('/', function(req,res){
-    res.render('index')
+    res.render('index', { serverPeople: people })
 })
 
 app.listen(port) 
--- public/js/app.js
+++ public/js/app.js
@@ -3,14 +3,6 @@ angular.module('TestApp', [])
 angular.module('TestApp').controller('MainController', ctrlFunc)
 
 function ctrlFunc(){
-    this.message = 'Hello'
 
-    this.people = [
-        {
-            name: 'Amine BENBAKHTA'
-        },
-        {
-            name: 'Rosane BENKHELIL'
-        }
-    ]
+    this.people = clientPeople
 }
